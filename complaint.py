135031664ab12067002f9f811a4056d8d0a0f971
shalabyelectronics <mhdshalabystar@gmail.com>
2021-11-03 02:48:49 +0300

    create complaint class to handle the questions.

---
+++ complaint.py
@@ -0,0 +1,26 @@
+
+class Complaint():
+    def __init__(self):
+        self.student_name = input("Write your first and last name, please : ").title()
+        self.instructor_name = input("What is your instructor name : ").title()
+        self.course_name = input("Which course are you currently doing? ").title()
+        self.unit_number = input("Which unit is your complaint related to it? ")
+        self.group_number = input("What is your group number?")
+        self.complaint_part = input(
+            "Which part do you want to complain about it?\n"
+            "A- Discussion forum\n"
+            "B- Written Assignment\n"
+            "C- Learning Journal\n"
+        )
+        self.issue = input(
+            "Why do you want to complain?\n"
+            "A- unfairly graded\n"
+            "B-Offensive comment\n"
+            "C- Others\n"
+        )
+
+
+
+
+
+
